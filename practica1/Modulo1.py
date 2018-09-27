c6efef652ec5891fe2b0af67c8e7b39d0726bd68
Dario <dariohdez16@gmail.com>
2018-09-27 17:07:55 +0200

    cosas

---
+++ practica1/Modulo1.py
@@ -0,0 +1,129 @@
+import matplotlib as matplotlib
+#%matplotlib inline
+#%load_ext autoreload
+#%autoreload 2
+
+import string, random
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import time
+
+import queue as qe
+
+from sklearn.linear_model import LinearRegression
+
+import networkx as nx
+
+
+def fit_plot(l, func_2_fit, size_ini, size_fin, step):
+    l_func_values = [i * func_2_fit(i) for i in range(size_ini, size_fin + 1, step)]
+
+    lr_m = LinearRegression()
+    X = np.array(l_func_values).reshape(len(l_func_values), -1)
+    lr_m.fit(X, l)
+    y_pred = lr_m.predict(X)
+
+    plt.plot(l, '*', y_pred, '-')
+
+
+def n2_log_n(n):
+    return n ** 2. * np.log(n)
+
+l = [
+[0, 10, 1, np.inf],
+[np.inf, 0, 1, np.inf],
+[np.inf, np.inf, 0, 1 ],
+[np.inf, 1, np.inf, 0]
+]
+
+m_g = np.array(l)
+
+def print_m_g(m_g):
+    print("graph_from_matrix:\n")
+    n_v = m_g.shape[0]
+    for u in range(n_v):
+        for v in range(n_v):
+            if v != u and m_g[u, v] != np.inf:
+                print("(", u, v, ")", m_g[u, v])
+
+d_g = {
+0: {1: 10, 2: 1},
+1: {2: 1},
+2: {3: 1},
+3: {1: 1}
+}
+
+def print_d_g(d_g):
+    print("\ngraph_from_dict:\n")
+    for u in d_g.keys():
+        for v in d_g[u].keys():
+            print("(", u, v, ")", d_g[u][v])
+
+
+#print_m_g(m_g)
+#print_d_g(d_g)
+
+def rand_matr_pos_graph(n_nodes, sparse_factor, max_weight=50., decimals=0):
+    """
+    """
+    grafo_completo = np.random.random_integers(1, max_weight, (n_nodes, n_nodes))
+    decimales = np.around(np.random.random_sample((n_nodes,n_nodes)),decimals)
+    ramas = np.random.binomial(1, sparse_factor, size=(n_nodes, n_nodes))  # dicta si hay ramas o no
+
+    grafo_completo = np.add(grafo_completo,decimales)
+
+    for u in range(n_nodes):
+        for v in range(n_nodes):
+            if ramas[u][v] == 1:
+                grafo_completo[u][v] = np.inf
+
+    np.fill_diagonal(grafo_completo, 0)
+    #print(grafo_completo)
+    #print("\n")
+    return grafo_completo
+
+def cuenta_ramas(m_g):
+    return len(np.flatnonzero(np.where(m_g == np.inf,0,1))) - m_g.shape[0]
+
+
+def check_sparse_factor(n_grafos,n_nodes, sparse_factor):
+    grafos = [None]*n_grafos
+    ramas = [None]*n_grafos
+    factores = [None]*n_grafos
+
+    for i in range(n_grafos):
+        grafos[i] = rand_matr_pos_graph(n_nodes,sparse_factor,10.,decimals=2)
+        ramas[i] = cuenta_ramas(grafos[i])
+        factores[i] = ramas[i] / (n_nodes**2 - n_nodes)
+
+    return 1 - np.average(factores)
+
+
+def m_g_2_d_g(m_g):
+    """
+    """
+    pass
+
+
+def d_g_2_m_g(d_g):
+    """
+    """
+    pass
+
+
+#m_g = rand_matr_pos_graph(n_nodes=5, sparse_factor=0.5, max_weight=10.,decimals = 2)
+# d_g = m_g_2_d_g(m_g)
+# m_g_2 = d_g_2_m_g(d_g)
+
+#print_m_g(m_g)
+#print(cuenta_ramas(m_g))
+print(check_sparse_factor(5,5,0.5))
+#mu1 = np.random.randint(0, 5, (5, 5))
+#mu2 = np.random.binomial(1, 0.5, size=(5, 5))
+#print(mu1)
+#print(mu2)
+# print_d_g(d_g)
+# print("\nnum_elem_iguales:\t%d" % (m_g_2 == m_g).sum() )
+
+
